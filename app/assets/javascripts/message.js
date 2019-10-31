d234d5bc85f3751b3cf79df13af86ccd752654d4
shiragakenichi <56780590+shiragakenichi@users.noreply.github.com>
2019-10-31 19:41:31 +0900

    ご指摘ありがとうございます
    6行目〜20行目
    インデント調整させていただきました。

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -3,65 +3,22 @@ $(function(){
       var addImage = message.image  ?  message.image : ''
       var addText = message.content ? message.content : ''
         var html = `<div class="message">
-                   <div class="message__upper-info">
-                   <div class="messge__upper-info__talker">
-                   ${message.name}
-                   </div>
-                   <div class="message__upper-info__date">
-                  ${message.created_at}
-                  </div>
-                  </div>
-                  <div class="message__text">
-                  <p class="message__text__content">
-                  ${message.content}
-                  </p>
-                  <img class="message__text__image" src="${addImage}">
-                  </div>
-                  </div>`
+                      <div class="message__upper-info">
+                        <div class="messge__upper-info__talker">
+                          ${message.name}
+                        </div>
+                        <div class="message__upper-info__date">
+                          ${message.created_at}
+                        </div>
+                      </div>
+                      <div class="message__text">
+                        <p class="message__text__content">
+                          ${message.content}
+                        </p>
+                        <img class="message__text__image" src="${addImage}">
+                      </div>
+                    </div>`
         return html;
-  
-    }
-  
-      $('#new_message').on('submit', function(e){
-        e.preventDefault()
-        var formData = new FormData(this);
-        var url = $(this).attr('action');
-        $.ajax({
-          url: url,  
-          type: 'POST', 
-          data: formData,  
-          dataType: 'json',
-          processData: false,
-          contentType: false,
-        })
-        .done(function(message){
-          var html = buildPost(message);
-          
-          $('.right-center').append(html)
-          $(`.new_message`)[0].reset();
-          $('.form__submit').prop('disabled', false);
-          $(function(){
-    function buildPost(message){
-      var addImage = message.image  ?  message.image : ''
-      var addText = message.content ? message.content : ''
-        var html = `<div class="message">
-                   <div class="message__upper-info">
-                   <div class="messge__upper-info__talker">
-                   ${message.name}
-                   </div>
-                   <div class="message__upper-info__date">
-                  ${message.created_at}
-                  </div>
-                  </div>
-                  <div class="message__text">
-                  <p class="message__text__content">
-                  ${message.content}
-                  </p>
-                  <img class="message__text__image" src="${addImage}">
-                  </div>
-                  </div>`
-        return html;
-  
     }
   
       $('#new_message').on('submit', function(e){
@@ -91,11 +48,3 @@ $(function(){
     })
   })
   
-          })
-        .fail(function(){
-          alert('メッセージ送信に失敗しました')
-    
-      })
-    })
-  })
-
