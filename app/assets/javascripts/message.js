3ccd897872ac505268690139b7e59676fc65d27c
shiragakenichi <56780590+shiragakenichi@users.noreply.github.com>
2019-10-31 17:26:37 +0900

    非同期通信完了です
    非同期通信完了いたしました。
    コードレビューおよび
    改善点などあれば、
    ご教授お願い致します。

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,53 @@
+$(function(){
+  function buildPost(message){
+    var addImage = message.image  ?  message.image : ''
+    var addText = message.content ? message.content : ''
+      var html = `<div class="message">
+                 <div class="message__upper-info">
+                 <div class="messge__upper-info__talker">
+                 ${message.name}
+                 </div>
+                 <div class="message__upper-info__date">
+                ${message.created_at}
+                </div>
+                </div>
+                <div class="message__text">
+                <p class="message__text__content">
+                ${message.content}
+                </p>
+                <img class="message__text__image" src="${addImage}">
+                </div>
+                </div>`
+      return html;
+
+  }
+
+    $('#new_message').on('submit', function(e){
+      e.preventDefault()
+      var formData = new FormData(this);
+      var url = $(this).attr('action');
+      $.ajax({
+        url: url,  //同期通信でいう『パス』
+        type: 'POST',  //同期通信でいう『HTTPメソッド』
+        data: formData,  
+        dataType: 'json',
+        processData: false,
+        contentType: false,
+      })
+      .done(function(message){
+        console.log(message)
+        var html = buildPost(message);
+        
+        $('.right-center').append(html)
+        //$('.form__message').val('')
+        $(`.new_message`)[0].reset();
+        $('.form__submit').prop('disabled', false);
+        
+        })
+      .fail(function(){
+        alert('メッセージ送信に失敗しました')
+  
+    })
+  })
+})
+
---
+++ app/views/messages/create.json.jbuilder
@@ -0,0 +1,4 @@
+json.content @message.content
+json.image @message.image.url
+json.created_at @message.created_at
+json.name @message.user.name
