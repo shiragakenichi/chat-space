ac562f90fd31db64051d53cb3d0d6e3a21b73d92
shiragakenichi <56780590+shiragakenichi@users.noreply.github.com>
2019-11-03 21:23:05 +0900

    自動更新機能の実装完了いたしました。
    自動更新機能の実装完了いたしました。
    変更点などありましたらご教授願えますでしょうか？
    ご確認よろしくお願い致します。

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,11 +1,12 @@
 $(function(){
     function buildPost(message){
-      var addImage = message.image  ?  message.image : ''
-      var addText = message.content ? message.content : ''
-        var html = `<div class="message">
+     
+      var addImage = message.image.url  ?  message.image.url : ''
+     
+        var html = `<div class="message" data-message-id="${message.id}">
                       <div class="message__upper-info">
                         <div class="messge__upper-info__talker">
-                          ${message.name}
+                          ${message.user_name}
                         </div>
                         <div class="message__upper-info__date">
                           ${message.created_at}
@@ -37,14 +38,43 @@ $(function(){
           var html = buildPost(message);
           
           $('.right-center').append(html)
-          $(`.new_message`)[0].reset();
+          $('.new_message')[0].reset();
           $('.form__submit').prop('disabled', false);
           
           })
         .fail(function(){
-          alert('メッセージ送信に失敗しました')
+          alert('メッセージ送信に失敗しました');
     
-      })
-    })
-  })
+      });
+    });
+     var reloadMessages = function () {
+         if (window.location.href.match(/\/groups\/\d+\/messages/)){//今いるページのリンクが/groups/グループID/messagesのパスとマッチすれば以下を実行。
+           var last_message_id = $('.message:last').data("message-id"); //dataメソッドで.messageにある:last最後のカスタムデータ属性を取得しlast_message_idに代入。
+          
+          console.log(last_message_id)
+           $.ajax({ //ajax通信で以下のことを行う
+             url: "api/messages", //サーバを指定。今回はapi/message_controllerに処理を飛ばす
+             type: 'get', //メソッドを指定
+             dataType: 'json', //データはjson形式
+             data: {last_id: last_message_id}
+             //飛ばすデータは先ほど取得したlast_message_id。またparamsとして渡すためlast_idとする。
+           })
+           .done(function (messages) { //通信成功したら、controllerから受け取ったデータ（messages)を引数にとって以下のことを行う
+             var insertHTML = '';//追加するHTMLの入れ物を作る
+             messages.forEach(function (message) {//配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+               insertHTML = buildPost(message); //メッセージが入ったHTMLを取得
+               $('.right-center').append(insertHTML);//メッセージを追加
+              })
+             $('.right-center').animate({scrollTop: $('.right-center')[0].scrollHeight}, 'fast');//最新のメッセージが一番下に表示されようにスクロールする。
+           })
+           .fail(function () {
+             alert('自動更新に失敗しました');//ダメだったらアラートを出す
+           });
+         }
+       };
+       setInterval(reloadMessages,5000);//5000ミリ秒ごとにreloadMessagesという関数を実行し自動更新を行う。
+});
+
+
+
   
