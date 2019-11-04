5c7182be2955469871a6c3454cdc6d72c9ec6c85
shiragakenichi <56780590+shiragakenichi@users.noreply.github.com>
2019-11-04 16:55:45 +0900

    lesson11
    lesson11

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+    
+  def index
+    return nil if params[:keyword] == ""
+    @users = User.where(['name LIKE ?', "%#{params[:keyword]}%"] ).where.not(id: params[:userid]).limit(10)
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
 
     def edit
     end
